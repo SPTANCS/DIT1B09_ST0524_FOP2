a9cc34cfe27eb13788f7e5204ffead43b638ac7f
example <example@example.com>
2023-11-08 12:27:09 +0800

    example 6 version 2

--- 04_JavaScript_Data_Structures_Part_1/example06.js
+++ 04_JavaScript_Data_Structures_Part_1/example06.js
@@ -4,15 +4,32 @@ for (let i=1; i<=10; i++) {
     numberArray.push(Math.floor(Math.random() * 100) + 1);
 }
 
+console.clear();
 console.log("Array: ", numberArray.toString());
 
+// let resultNumber = numberArray.reduce((currentHighestValue, currentValue, currentIndex, array) => {
+//     if (currentHighestValue > currentValue) { // the currentHighestValue is still the highest value
+//         return currentHighestValue;
+//     } else { // the currentHighestValue is no longer the highest value it should be replace by 
+//         return currentValue;
+//     }
+// });
 
 
+// let resultNumber = numberArray.reduce((currentHighestValue, currentValue) => {
+//     if (currentHighestValue > currentValue) { // the currentHighestValue is still the highest value
+//         return currentHighestValue;
+//     } else { // the currentHighestValue is no longer the highest value it should be replace by 
+//         return currentValue;
+//     }
+// });
 
+// let resultNumber = numberArray.reduce((currentHighestValue, currentValue) => {
+//     return (currentHighestValue > currentValue) ? currentHighestValue : currentValue;
+// });
 
+let resultNumber = numberArray.reduce((currentHighestValue, currentValue) =>    
+    (currentHighestValue > currentValue) ? currentHighestValue : currentValue
+);
 
-// let resultNumber = numberArray.reduce((highestValue, currentValue) =>    
-//     (currentValue > highestValue) ? currentValue : highestValue
-// );
-
-// console.log(`Highest number in array is ${resultNumber}`);
+console.log(`Highest number in array is ${resultNumber}`);
